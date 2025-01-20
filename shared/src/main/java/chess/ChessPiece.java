623d0b13ea90895cf017f18dd95aeadff5d9a681
Sapere Foods <saperefoods@gmail.com>
2025-01-20 16:16:33 -0700

    generating  equals() and hashcode()

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -15,6 +16,21 @@ public class ChessPiece {
     }
     private ChessGame.TeamColor teamColor;
     private ChessPiece.PieceType pieceType;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return teamColor == that.teamColor && pieceType == that.pieceType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamColor, pieceType);
+    }
+
     /**
      * The various different chess piece options
      */
